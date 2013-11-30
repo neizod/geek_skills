ab3d53cffe87166056e8bf52b1bf29f579c19b97
Nattawut Phetmak <neizod@gmail.com>
2013-11-30 11:04:01 +0700

    make meta layout for extended

---
+++ app/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    @yield('hook-head')
+  </head>
+
+  <body>
+    <h1>Geek Skill Tree</h1>
+
+    @yield('content')
+  </body>
+
+</html>
--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -1,47 +1,50 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      div.mock.col3 {
-        display: inline-block;
-        padding:  1em;
-        width:    30%;
-      }
-      div.mock.col3 h2 {
-        border-width: thin;
-        border-style: dashed;
-        padding:  1em;
-        height: 200px;
-      }
+@extends('layout')
 
-      div.attension {
-        text-align: center;
-      }
-      div.attension h1 {
-        display: inline-block;
-        padding:  0.5em;
-        border-width: medium;
-        border-style: solid;
-      }
-    </style>
-  <head>
-  <body>
-    <h1>Geek Skill Tree</h1>
 
-    @foreach(array('skill tree', 'language+framework', 'achievements') as $item)
-    <div class="mock col3">
-      <h2>{{ $item }}</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    </div>
-    @endforeach
+@section('hook-head')
 
-    <div class="attension">
-      <h1>create account now!</h1>
-      <br />(or login if you already have an account)
-    </div>
+  <style>
+    div.mock.col3 {
+      display: inline-block;
+      padding:  1em;
+      width:    30%;
+    }
+    div.mock.col3 h2 {
+      border-width: thin;
+      border-style: dashed;
+      padding:  1em;
+      height: 200px;
+    }
+  
+    div.attension {
+      text-align: center;
+    }
+    div.attension h1 {
+      display: inline-block;
+      padding:  0.5em;
+      border-width: medium;
+      border-style: solid;
+    }
+  </style>
 
-  </body>
-</html>
+@stop
+
+
+@section('content')
+
+  @foreach(array('skill tree', 'language+framework', 'achievements') as $item)
+  <div class="mock col3">
+    <h2>{{ $item }}</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+  @endforeach
+  
+  <div class="attension">
+    <h1>create account now!</h1>
+    <br />(or login if you already have an account)
+  </div>
+
+@stop
