8158f3001673054371b46c674f23c09b8130e852
Nattawut Phetmak <neizod@gmail.com>
2013-11-30 10:53:12 +0700

    test make skill img

--- app/views/skills.blade.php
+++ app/views/skills.blade.php
@@ -7,9 +7,13 @@
       }
       button[name='sid'] {
         position: absolute;
+        display: inline-block;
         padding: 0px;
         border:  0px;
         margin:  0px;
+        border-style: solid;
+        border-width: thin;
+        border-color: #09f;
       }
       button[value='1']  { top:   0px; left: 125px; }
       button[value='2']  { top: 125px; left: 250px; }
@@ -29,13 +33,19 @@
       button[value='16'] { top: 375px; left:   0px; }
       button[value='17'] { top:   0px; left:   0px; }
       button[value='18'] { top: 500px; left: 250px; }
+      button img {
+        height: 78px;
+        width:  78px;
+      }
     </style>
   </head>
   <body>
   <h1>Geek Skill Tree</h1>
   <div class="container">
     @for ($i=18; $i; $i--)
-    <button name="sid" value="{{$i}}">skill #{{$i}}</button>
+    <button name="sid" value="{{$i}}">
+      {{ HTML::image("public/img/s$i.jpg") }}
+    </button>
     @endfor
   </div>
   </body>
