84e6748c74152318b9f98b03cf0f9076a5e2f636
Nattawut Phetmak <neizod@gmail.com>
2014-02-17 03:22:06 +0700

    make model relations

--- app/models/Skill.php
+++ app/models/Skill.php
@@ -4,4 +4,8 @@ class Skill extends Eloquent {
 
     protected $fillable = array('abbr', 'name');
 
+    public function prerequisites() {
+        return $this->belongsToMany('Skill', 'prerequisites', 'rid', 'sid');
+    }
+
 }
--- app/models/User.php
+++ app/models/User.php
@@ -4,4 +4,8 @@ class User extends Eloquent {
 
     protected $fillable = array('name', 'gh_id');
 
+    public function skills() {
+        return $this->belongsToMany('Skill', 'learnings', 'uid', 'sid');
+    }
+
 }
